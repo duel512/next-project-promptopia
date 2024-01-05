668ac2b0321eed1ecfe0f75260e5a3b3b9fa19b4
Ruomu <shaoruomu19@gmail.com>
2024-01-05 22:40:47 +1100

    Components added. Nav bar basiclly set up

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,24 +1,26 @@
-import '@styles/global.css';
+import "@styles/globals.css";
+
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
-    title: "Promptopia",
-    description: "Discover & Share AI prompts"
-}
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
+};
 
-const RootLayout = ({ children }) => {
-  return (
-    <html lang='en'>
-        <body>
-            <div className='main'>
-                <div className='gradient' />
+const RootLayout = ({ children }) => (
+  <html lang='en'>
+    <body>
+        <div className='main'>
+          <div className='gradient' />
+        </div>
 
-                <main className='app'>
-                    {children}
-                </main>
-            </div>
-        </body>
-    </html>
-  )
-}
+        <main className='app'>
+          <Nav />
+          {children}
+        </main>
+    </body>
+  </html>
+);
 
 export default RootLayout;
