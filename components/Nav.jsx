53c741fbad3cb6092b57c4fc99530f62d7bb46e7
Ruomu <shaoruomu19@gmail.com>
2024-01-09 18:54:11 +1100

    tesing for Vercel not showing signing in

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -33,7 +33,7 @@ const Nav = () => {
 
         <p className="logo_text">Promptopia</p>
       </Link>
-
+      {alert({session})}
       {/* Desktop Navigation */}
       <div className="sm:flex hidden">
           {
