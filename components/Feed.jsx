22f7ca88006cc3285252f1d53425c6741edeceae
Ruomu Shao <ruomushao@192-168-1-14.tpgi.com.au>
2024-01-16 17:19:05 +1100

    update on MAC

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -6,7 +6,7 @@ import PromptCard from "./PromptCard";
 
 const PromptCardList = ({ data, handleTagClick }) => {
   return (
-    <div className='mt-16 prompt_layout'>
+    <div className="mt-16 prompt_layout">
       {data.map((post) => (
         <PromptCard
           key={post._id}
@@ -19,45 +19,54 @@ const PromptCardList = ({ data, handleTagClick }) => {
 };
 
 const Feed = () => {
-  const [searchText, setSearchText] = useState('');
+  const [searchText, setSearchText] = useState("");
   const [posts, setPosts] = useState([]);
-  const [searchedPosts, setSearchedPosts] = useState([])
+  const [searchedPosts, setSearchedPosts] = useState([]);
 
-  const fetchPosts = async ()=> {
-    const response = await fetch('/api/prompt')
+  const fetchPosts = async () => {
+    const response = await fetch("/api/prompt");
     const data = await response.json();
 
     setPosts(data);
     setSearchedPosts(data);
-   }
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchPosts();
-  }, [])
+  }, []);
 
   const handleSearchChange = (e) => {
-    const text = e.target.value
-    setSearchText(text)
+    const text = e.target.value;
+    setSearchText(text);
 
-    if(text){
+    if (text) {
       const filtered = posts.filter((post) => {
-            const idMatch = post.creator ? post.creator.username.includes(text) : false;
-            const promptMatch = post.prompt ? post.prompt.includes(text) : false;
-            const tagMatch = post.tag ? post.tag.includes(text) : false;
-            return idMatch || promptMatch || tagMatch;
-          });
+        const idMatch = post.creator
+          ? post.creator.username.includes(text)
+          : false;
+        const promptMatch = post.prompt ? post.prompt.includes(text) : false;
+        const tagMatch = post.tag ? post.tag.includes(text) : false;
+        return idMatch || promptMatch || tagMatch;
+      });
 
-          setSearchedPosts(filtered);
-    } else{
+      setSearchedPosts(filtered);
+    } else {
       setSearchedPosts(posts);
     }
-    
-  }
+  };
+
+  const handleTagClick = (tag) => {
+    const filtered = posts.filter((post) => {
+      return post.tag === tag;
+    });
+
+    setSearchedPosts(filtered);
+  };
 
   return (
     <section className="feed">
       <form className="relative w-full flex-center">
-        <input  
+        <input
           type="text"
           placeholder="Search for a username, tag, or prompt content"
           value={searchText}
@@ -67,12 +76,9 @@ const Feed = () => {
         />
       </form>
 
-      <PromptCardList
-        data={searchedPosts}
-        handleTagClick={()=>{}}
-      />
+      <PromptCardList data={searchedPosts} handleTagClick={handleTagClick} />
     </section>
-  )
-}
+  );
+};
 
-export default Feed
+export default Feed;
