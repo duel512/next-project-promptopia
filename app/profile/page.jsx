21b1120d6f62ea5a7ffb3f166f9a38dfa115e99b
Ruomu <shaoruomu19@gmail.com>
2024-01-16 20:44:37 +1100

    testing for Vercel not showing all prompts on Home page

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,60 +1,59 @@
-"use client"
+"use client";
 
-import { useState, useEffect} from 'react'
-import { useSession } from 'next-auth/react'
-import { useRouter } from 'next/navigation'
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
-import Profile from '@components/Profile'
+import Profile from "@components/Profile";
 
 const MyProfile = () => {
-    const router = useRouter();
-    const { data: session } = useSession();
+  const router = useRouter();
+  const { data: session } = useSession();
 
-    const [myPosts, setMyPosts] = useState([]);
+  const [myPosts, setMyPosts] = useState([]);
 
-    const fetchPosts = async ()=> {
-        const response = await fetch(`/api/users/${session?.user.id}/posts`)
-        const data = await response.json();
-    
-        setMyPosts(data);
-       }
-    
-      useEffect(()=>{
-        if(session?.user.id)
-            fetchPosts();
-      }, [])
+  const fetchPosts = async () => {
+    const response = await fetch(`/api/users/${session?.user.id}/posts`);
+    const data = await response.json();
 
-    const handleEdit = (post) => {
-      router.push(`/update-prompt?id=${post._id}`);
-    }
+    setMyPosts(data);
+  };
 
-    const handleDelete = async (post) => {
-      const hasConfirmed = confirm("Are you sure to Delete this prompt?")
-      
-      if(hasConfirmed){
-        try {
-          await fetch(`/api/prompt/${post._id.toString()}`, {
-            method: 'DELETE'
-          });
+  useEffect(() => {
+    if (session?.user.id) fetchPosts();
+  }, []);
 
-          const filteredPosts = myPosts.filter((p) => p._id !== post._id);
-          
-          setMyPosts(filteredPosts);
-        } catch (error) {
-          console.log(error);
-        }
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
+
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm("Are you sure to Delete this prompt?");
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: "DELETE",
+        });
+
+        const filteredPosts = myPosts.filter((p) => p._id !== post._id);
+
+        setMyPosts(filteredPosts);
+      } catch (error) {
+        console.log(error);
       }
     }
+  };
 
   return (
     <Profile
-        name="My"
-        desc="Welcome to your own MyProfile page!"
-        data={myPosts}
-        handleEdit={handleEdit}
-        handleDelete={handleDelete}
+      name={session?.user.name}
+      desc="Welcome to your own MyProfile page!"
+      data={myPosts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
     />
-  )
-}
+  );
+};
 
-export default MyProfile
+export default MyProfile;
